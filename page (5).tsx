da244672e9a2e3f2c91c0d19bf218171ac0b212f
FrancoAnyx <166439630+FrancoAnyx@users.noreply.github.com>
2026-04-01 13:48:42 -0300

    Delete page (5).tsx

--- page (5).tsx
+++
@@ -1,4 +0,0 @@
-import Navbar from '@/components/layout/Navbar'
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen flex flex-col"><Navbar /><main className="flex-1">{children}</main></div>
-}
