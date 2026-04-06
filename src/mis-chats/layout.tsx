3204f9da233657fb0a2edf9e54e6484b3bf84511
FrancoAnyx <166439630+FrancoAnyx@users.noreply.github.com>
2026-04-06 16:25:44 -0300

    Delete src/mis-chats directory

--- src/mis-chats/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '@/components/layout/Navbar'
-export default function MisChatsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-1" style={{ paddingTop: 64 }}>{children}</main>
-    </div>
-  )
-}
